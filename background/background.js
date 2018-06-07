0ce48b17627b4e6150066decdd12322fc68c3219
awangg <andy_wang@student.wayland.k12.ma.us>
2018-06-07 11:11:05 -0400

    changed background to add default tabs to "links" variable instead of "savedtabs"

--- background/background.js
+++ background/background.js
@@ -1,7 +1,7 @@
 var tabs = deftabs;
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({enableTab: true, title: 'Waytab', savedtabs: tabs.tabs}, function() {
+  chrome.storage.sync.set({enableTab: true, title: 'Waytab', links: tabs.tabs}, function() {
     console.log("Installed Tab Changer");
   });
 });
