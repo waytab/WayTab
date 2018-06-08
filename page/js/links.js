257afa6ba5b1c14144b7c132e4484e0deebc1f56
awangg <andy_wang@student.wayland.k12.ma.us>
2018-06-07 20:55:54 -0400

    Added remove-link functionality - check my jquery @andrewward2001

--- page/js/links.js
+++ page/js/links.js
@@ -2,6 +2,7 @@ export class Links {
   constructor() {
     let linksFunc = this.loadLinks
     let addFunc = this.addLink;
+    let removFunc = this.enableRemove;
     chrome.storage.sync.get(['links'], function(result) {
       if(Object.keys(result).length === 0 && result.constructor === Object) {
         $.getJSON('js/json/config.json', (data) => {
@@ -14,12 +15,14 @@ export class Links {
       }
     })
     addFunc(linksFunc);
+    removFunc();
   }
 
   loadLinks(obj) {
     for(let i = 0; i < obj.length; i++) {
       $('#link-container').append($('<div></div>').addClass('col-1').attr('id', `link${i}`))
       $(`#link${i}`).append($('<a></a>').addClass('img-link').attr('href', obj[i].actual_link).append($('<img />').attr({ src: obj[i].image_link, alt: obj[i].id })))
+      $(`#link${i}`).append($('<div>X</div>').addClass('tab-delete-button').attr('id', `delete${i}`))
     }
     $('#link-container').prepend($('<div></div>').addClass('col')).append($('<div></div>').addClass('col'))
   }
@@ -38,14 +41,29 @@ export class Links {
       chrome.storage.sync.get(['links'], function(result) {
         console.log(result.links);
         result.links.push(obj);
-        chrome.storage.sync.set({links: result.links}, () => {
-          console.log("Added tab to background");
-        });
+        chrome.storage.sync.set({links: result.links}, () => {});
       });
     });
   }
 
   enableRemove() {
-
+    $(document).on('click', '#remove-button', () => {
+      if($('#remove-button').attr('aria-expanded') === "true") {
+        $('#link-container .col-1').each(function(index) {
+          $('#delete'+index).css('display', 'block');
+          $('#delete'+index).on('click', () => {
+            $(`#link${index}`).remove();
+            chrome.storage.sync.get(['links'], function(result) {
+              result.links.splice(index, 1);
+              chrome.storage.sync.set({links: result.links}, () => {});
+            });
+          });
+        });
+      }else {
+        $('#link-container .col-1').each(function(index) {
+          $('#delete'+index).css('display', 'none');
+        });
+      }
+    })
   }
 }
