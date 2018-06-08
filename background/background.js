d564876bd75ab1786ca3a5cfca3c99ebcef86ed3
awangg <andy_wang@student.wayland.k12.ma.us>
2018-06-08 08:27:24 -0400

    Added background changing

--- background/background.js
+++ background/background.js
@@ -1,7 +1,7 @@
 var tabs = deftabs;
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({enableTab: true, title: 'WayTab', links: tabs.tabs}, function() {
+  chrome.storage.sync.set({enableTab: true, title: 'WayTab', links: tabs.tabs, background: './img/school.jpg'}, function() {
     console.log("Installed Tab Changer");
   });
 });
--- page/js/hide.js
+++ page/js/hide.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
   hideBody();
+  loadBackground();
+  changeBackground();
 })
 
 function hideBody() {
@@ -11,5 +13,20 @@ function hideBody() {
       $('#content').css('display', 'none');
       $('#content-status').text('Show Content');
     }
+  });
+}
+
+function loadBackground() {
+  chrome.storage.sync.get(['background'], function(response) {
+    $(document.body).css('background-image', 'url(\"'+response.background+'\")');
+  });
+}
+
+function changeBackground() {
+  $(document).on('click', '#submit-new-background', () => {
+    let sel = $('#select-background').val()[0];
+    let href = './img/' + sel.toLowerCase()+'.jpg';
+    $(document.body).css('background-image', 'url(\"'+href+'\")');
+    chrome.storage.sync.set({background: href}, function(response) {});
   });
 }
