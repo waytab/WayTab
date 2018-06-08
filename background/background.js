2ebb1c9144b177d0f5e8da53c46d7236bf885293
awangg <andy_wang@student.wayland.k12.ma.us>
2018-06-08 07:41:55 -0400

    Changed default title to WayTab

--- background/background.js
+++ background/background.js
@@ -1,7 +1,7 @@
 var tabs = deftabs;
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({enableTab: true, title: 'Waytab', links: tabs.tabs}, function() {
+  chrome.storage.sync.set({enableTab: true, title: 'WayTab', links: tabs.tabs}, function() {
     console.log("Installed Tab Changer");
   });
 });
