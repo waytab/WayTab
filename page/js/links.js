febaf0cf101dd65a502d4d4203c27537aa2c4a89
awangg <andy_wang@student.wayland.k12.ma.us>
2018-06-07 20:27:01 -0400

    Added add-link functionality - i hope ur happy with my jquery ward

--- page/js/links.js
+++ page/js/links.js
@@ -1,6 +1,7 @@
 export class Links {
   constructor() {
     let linksFunc = this.loadLinks
+    let addFunc = this.addLink;
     chrome.storage.sync.get(['links'], function(result) {
       if(Object.keys(result).length === 0 && result.constructor === Object) {
         $.getJSON('js/json/config.json', (data) => {
@@ -12,6 +13,7 @@ export class Links {
         linksFunc(result.links);
       }
     })
+    addFunc(linksFunc);
   }
 
   loadLinks(obj) {
@@ -20,5 +22,30 @@ export class Links {
       $(`#link${i}`).append($('<a></a>').addClass('img-link').attr('href', obj[i].actual_link).append($('<img />').attr({ src: obj[i].image_link, alt: obj[i].id })))
     }
     $('#link-container').prepend($('<div></div>').addClass('col')).append($('<div></div>').addClass('col'))
+  }
+
+  addLink(loadFunc) {
+    $(document).on('click', '#submit-tab-info', () => {
+      let name = $('#tab-name').val();
+      let link = $('#tab-link').val();
+      let img = $('#img-upload').val();
+      if(img.length <= 8) {
+        img = './img/default.png'
+      }
+      let obj = [{"name": name, "actual_link": link, "image_link": img}];
+      loadFunc(obj);
+
+      chrome.storage.sync.get(['links'], function(result) {
+        console.log(result.links);
+        result.links.push(obj);
+        chrome.storage.sync.set({links: result.links}, () => {
+          console.log("Added tab to background");
+        });
+      });
+    });
+  }
+
+  enableRemove() {
+
   }
 }
