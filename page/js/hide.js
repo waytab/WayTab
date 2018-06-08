1c929ebbdcb50e9e2ffd4a92f3fb042d2067fba6
awangg <andy_wang@student.wayland.k12.ma.us>
2018-06-08 14:27:39 -0400

    Content automatically hides after 1 minute of idle time

--- page/js/hide.js
+++ page/js/hide.js
@@ -1,8 +1,26 @@
+var idle = 0;
+
 $(document).ready(() => {
+  let idleInterval = setInterval(idleHide, 60000);
+  $(this).mousemove(function (e) {
+    idle = 0;
+  });
+  $(this).keypress(function (e) {
+    idle = 0;
+  });
+
   hideBody();
   loadBackground();
   changeBackground();
-})
+});
+
+function idleHide() {
+  idle += 1;
+  if(idle >= 1) {
+    $('#content').css('display', 'none');
+    $('#content-status').text('Show Content');
+  }
+}
 
 function hideBody() {
   $(document).on('click', '#hide-content', () => {
