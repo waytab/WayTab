d7d5ca8669cf2229f1e694296478a05664decb06
Andrew Ward <tmiandreww@gmail.com>
2018-06-09 14:03:45 -0400

    Title is now a class

--- page/js/title.js
+++ page/js/title.js
@@ -1,21 +1,16 @@
-$(document).ready(() => {
-  loadTitle();
-  changeTitle();
-})
-
-function loadTitle() {
-  chrome.storage.sync.get("title", function(response) {
-    $('#title-container').text(response.title);
-    $('#title-input').val(response.title)
-  });
-}
-
-function changeTitle() {
-  $(document).on('click', '#settings-close', function() {
-    let newtitle = $('#title-input').val();
-    chrome.storage.sync.set({title: newtitle}, function() {
-      $('#title-container').text(newtitle);
-      console.log("New Title Set");
+export class Title {
+  constructor() {
+    chrome.storage.sync.get("title", function(response) {
+      $('#title-container').text(response.title)
+      $('#title-input').val(response.title)
     })
-  })
+
+    $(document).on('click', '#settings-close', function() {
+      let newtitle = $('#title-input').val()
+      chrome.storage.sync.set({title: newtitle}, function() {
+        $('#title-container').text(newtitle)
+        console.log("New Title Set")
+      })
+    })
+  }
 }
