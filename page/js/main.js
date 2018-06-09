de75c8f5647505205e43039bf20e98248d0e8a51
Andrew Ward <tmiandreww@gmail.com>
2018-06-09 14:04:08 -0400

    Add title and background to main

--- page/js/main.js
+++ page/js/main.js
@@ -1,7 +1,11 @@
+import { Background } from './background.js'
 import { Links } from './links.js'
 import { Schedule } from './schedule.js'
+import { Title } from './title.js'
 import { Todo } from './todo.js'
 
+let background = new Background()
 let links = new Links()
 let schedule = new Schedule()
+let title = new Title()
 let todo = new Todo()
