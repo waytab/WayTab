2946feb3e645fbb0548c72cfe79c5f340c146a5f
Andrew Ward <tmiandreww@gmail.com>
2018-06-09 22:27:31 -0400

    Add ability to edit links

--- page/js/links.js
+++ page/js/links.js
@@ -14,6 +14,7 @@ export class Links {
     })
     this.addLink()
     this.removeLinks()
+    this.editLink()
   }
 
   loadLinks(obj) {
@@ -29,7 +30,26 @@ export class Links {
       $(`#link${i}`).append($('<a></a>').addClass('img-link').attr('href', obj[i].actual_link).append($('<img />').attr({ src: obj[i].image_link, alt: obj[i].name })))
       $(`#link${i}`).append($('<div>X</div>').addClass('tab-delete-button').attr('id', `delete${i}`))
 
-      $('#remove-links').append($('<li></li>').addClass('list-group-item').html(`<button class="mr-3 delete-link" data-num="${i}" style="background: none; border: none; cursor: pointer;">&times;</button>${obj[i].name}`))
+      $('#remove-links')
+        .append($('<li></li>')
+          .addClass('list-group-item d-inline-flex')
+          .append(
+            $('<button></button>')
+              .addClass('mr-3 delete-link')
+              .attr('data-num', i)
+              .css({
+                background: 'none',
+                border: 'none',
+                cursor: 'pointer'
+              })
+              .html('&times;'),
+            $('<div></div>')
+              .addClass('link-edit')
+              .css('cursor', 'pointer')
+              .data('edit', i)
+              .text(obj[i].name)
+          )
+        )
     }
     $('#link-container').prepend($('<div></div>').addClass('col')).append($('<div></div>').addClass('col'))
   }
@@ -60,6 +80,40 @@ export class Links {
         result.links.splice(targetLink, 1)
         chrome.storage.sync.set({'links': result.links}, () => { this.loadLinks(result.links) })
       })
+    })
+  }
+
+  editLink() {
+    $(document).on('click', '.link-edit', (e) => {
+      $(e.target)
+        .addClass('w-100')
+        .text('')
+        .append(
+          $('<input>')
+            .addClass('form-control')
+            .attr('placeholder', 'Name')
+            .val($(`#link${$(e.target).data('edit')} a img`).attr('alt')),
+          $('<input>')
+            .addClass('form-control')
+            .attr('placeholder', 'URL')
+            .val($(`#link${$(e.target).data('edit')} a`).attr('href')),
+          $('<input>')
+            .addClass('form-control')
+            .attr('placeholder', 'Image URL')
+            .val($(`#link${$(e.target).data('edit')} a img`).attr('src'))
+        )
+    })
+
+    $(document).on('click', '#settings-close', (e) => {
+      chrome.storage.sync.get(['links'], function(result) {
+        let links = result.links
+        $('.link-edit').each(function(index) {
+          if($(this).has('input').length){
+            links[index] = { name: $(this).children('input').eq(0).val(), actual_link: $(this).children('input').eq(1).val(), image_link: $(this).children('input').eq(2).val() }
+          }
+        })
+        chrome.storage.sync.set({links: links})
+      });
     })
   }
 }
