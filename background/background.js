9c10dd169b9d609b14c42c3515df66fc17c15e4c
Andrew Ward <tmiandreww@gmail.com>
2018-06-10 21:34:43 -0400

    Add setting to change the font family

--- background/background.js
+++ background/background.js
@@ -1,7 +1,7 @@
 var tabs = deftabs;
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({enableTab: true, title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false}, function() {
+  chrome.storage.sync.set({enableTab: true, title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default'}, function() {
     console.log("Installed Tab Changer");
   });
 });
--- page/js/main.js
+++ page/js/main.js
@@ -1,8 +1,10 @@
+// these should be alphabetical...
 import { Background } from './background.js'
 import { Links } from './links.js'
 import { Schedule } from './schedule.js'
 import { Title } from './title.js'
 
+// ...and so should these
 let background = new Background()
 let links = new Links()
 let schedule = new Schedule()
@@ -12,4 +14,18 @@ let title = new Title()
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
+})
+
+// this part is commented to display a general flow of how settings are loaded.
+$(document).ready(function() { // we need to wait for the full document to be loaded before we start working on it
+  chrome.storage.sync.get(['font'], function(result) { // grabs the 'font' variable from chrome's storage, then passes it to an anonymous function
+    if(result.font != 'default') { // we only need to do stuff if the font isn't the default one
+      $('body').addClass(`font-${result.font}`) // we set the body to use the saved font family
+      $('#announcementsPositionSel').val(result.font) // we set the settings drop-down to the selected style. not required but a nice touch
+    }
+  })
+})
+
+$(document).on('click', '#settings-close', function() { // we need to save the new font style when the modal is closed
+  chrome.storage.sync.set({font: $('#announcementsPositionSel').val()}, function() { console.log('New font set') })
 })
