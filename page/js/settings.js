55fa2ec6478d4f73ba8caa2126a2dc244ff9f223
Andy Wang <andy_wang@student.wayland.k12.ma.us>
2018-09-19 21:01:18 -0400

    existing task NAMES will now be printed onto the console following force reset

--- page/js/settings.js
+++ page/js/settings.js
@@ -19,6 +19,18 @@ function revealDangerZone() {
 
 function hardResetTasks() {
   $(document).on('click', '#force-reset-tasks', function() {
+    chrome.storage.sync.get('tasks', function(res) {
+      if(res.tasks !== null) {
+        Object.keys(res.tasks).forEach( function(obj) {
+          // I console log all of the task NAMES so that they can re-enter assignments after the reset
+          console.log(obj + ":")
+          for(i = 0; i < this.length; i++) {
+            console.log(this[i][0])
+          }
+          console.log('-----')
+        })
+      }
+    })
     chrome.storage.sync.set({tasks: {}}, function() {
     })
     $('#taskList').empty()
