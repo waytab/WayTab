0530869f2b39a9a2ed40802faa88ab29f62e6cf0
Andrew Ward <andrew_ward@student.wayland.k12.ma.us>
2018-09-21 12:02:20 -0400

    Possible fix for #27
    needs testing

--- background/background.js
+++ background/background.js
@@ -1,7 +1,8 @@
 var tabs = deftabs;
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({enableTab: true, title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true}, function() {
-    console.log("Installed Tab Changer");
-  });
+chrome.runtime.onInstalled.addListener(function(deets) {
+  chrome.storage.sync.set({enableTab: true}, function() {});
+  if(deets.reason == 'install') {
+    chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true}, function() {});
+  }
 });
