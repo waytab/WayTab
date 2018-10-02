913258e7cda0f26e992bb87bf35e83fbf5737743
Andy Wang <andy_wang@student.wayland.k12.ma.us>
2018-10-01 20:08:04 -0400

    removed the adding classroom thing, not necessary after 1.1.1 update

--- background/background.js
+++ background/background.js
@@ -5,14 +5,6 @@ chrome.runtime.onInstalled.addListener(function(deets) {
   if(deets.reason == 'install') {
     chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true}, function() {});
   } else if (deets.reason == 'update') {
-    chrome.storage.sync.get(['links'], (r) => {
-      let l = r.links
-      l.splice(2, 0, {
-        "name": "Classroom",
-        "actual_link": "https://classroom.google.com",
-        "image_link": "https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png"
-      })
-      chrome.storage.sync.set({links: l})
-    })
+    // Empty cuz we don't need to add anything here for 1.1.2
   }
 });
