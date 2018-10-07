5d843550b16e6d01d433c8df58efebd55aa1e048
Andy Wang <andy_wang@student.wayland.k12.ma.us>
2018-10-07 12:04:40 -0400

    no more semicolons

--- background/background.js
+++ background/background.js
@@ -1,9 +1,9 @@
 var tabs = deftabs;
 
 chrome.runtime.onInstalled.addListener(function(deets) {
-  chrome.storage.sync.set({enableTab: true}, function() {});
+  chrome.storage.sync.set({enableTab: true}, function() {})
   if(deets.reason == 'install') {
-    chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true, bell2: false, todoDate: 'Tomorrow'}, function() {});
+    chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true, bell2: false, todoDate: 'Tomorrow'}, function() {})
   } else if (deets.reason == 'update') {
     chrome.storage.sync.set({todoDate: 'Tomorrow'})
   }
