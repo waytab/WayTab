b5f9523672e6853213ff1161ec83acea5dae4464
Andy Wang <andy_wang@student.wayland.k12.ma.us>
2018-10-09 21:52:50 -0400

    set new naviance link on update

--- background/background.js
+++ background/background.js
@@ -5,6 +5,10 @@ chrome.runtime.onInstalled.addListener(function(deets) {
   if(deets.reason == 'install') {
     chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true, bell2: false, todoDate: 'Tomorrow'}, function() {});
   } else if (deets.reason == 'update') {
-    chrome.storage.sync.set({todoDate: 'Tomorrow'})
+    chrome.storage.sync.get('links', function(res) {
+      let updatedLinks = res.links
+      updatedLinks[6].actual_link = 'https://student.naviance.com/waylandhs'
+      chrome.storage.sync.set({links: updatedLinks}, function() {})
+    })
   }
 });
