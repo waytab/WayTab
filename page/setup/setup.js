d67cd325bef1913c9f360095f3262cea3bb11705
Andrew Ward <tmiandreww@gmail.com>
2018-10-12 08:39:25 -0400

    next button functionality

--- page/setup/setup.js
+++ page/setup/setup.js
@@ -1,6 +1,22 @@
+let currentTab = 0
+let tabList = ['about', 'personalize', 'links', 'schedule']
+
 $(document).ready(() => {
   $('#setupModal').modal({
     backdrop: false,
     keyboard: false
   })
+})
+
+$(document).on('click', '#next', () => {
+  currentTab++
+  let nextTab = $(`#${tabList[currentTab]}-tab`)
+  nextTab.removeClass('disabled')
+
+  $(`#${tabList[currentTab]}-tab`).tab('show')
+})
+
+$(document).on('click', '[data-toggle="tab"]', function () {
+  console.log($(this));
+  currentTab = tabList.indexOf($(this).attr('href').substring(1))
 })
