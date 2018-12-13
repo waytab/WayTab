e5bdeb1313782248ce7652892d74038599090486
Andy Wang <andy_wang@student.wayland.k12.ma.us>
2018-12-13 17:50:15 -0500

    redid the getjson stuff in background cuz it was bad before

--- background/background.js
+++ background/background.js
@@ -1,17 +1,13 @@
-var tabs = deftabs;
+let tabs
 
-chrome.runtime.onInstalled.addListener(function(deets) {
-  chrome.storage.sync.set({enableTab: true}, function() {});
-  if(deets.reason == 'install') {
-    chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true, bell2: false, todoDate: 'Tomorrow'}, function() {});
-  } else if (deets.reason == 'update') {
-    chrome.storage.sync.get('links', function({links}) {
-      links.forEach(e => {
-        if(e.actual_link == 'https://connection.naviance.com/family-connection/auth/login/?hsname=waylandhs') {
-          e.actual_link = 'https://student.naviance.com/waylandhs'
-        }
-      })
-      chrome.storage.sync.set({links: links}, function() {})
-    })
-  }
-});
+$.getJSON('./defaulttabs.json', function(data) {
+  tabs = data
+  chrome.runtime.onInstalled.addListener(function(deets) {
+    chrome.storage.sync.set({enableTab: true}, function() {});
+    if(deets.reason == 'install') {
+      chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true, bell2: false, todoDate: 'Tomorrow', startChangeLog: true}, function() {});
+    } else if (deets.reason == 'update') {
+      chrome.storage.sync.set({startChangeLog: true}, function() {})
+    }
+  });
+})
