10f0b0f647059bb94f488c28c10937107aa7c0b8
Andrew Ward <tmiandreww@gmail.com>
2019-01-31 15:51:23 -0500

    this should never have been in reset.js

--- page/js/reset.js
+++ page/js/reset.js
@@ -49,39 +49,3 @@ function hardResetWaytab() {
     })
   })
 }
-
-// settings button / action menu
-let opened = false
-let startWidth
-setTimeout(() => { startWidth = $('.fab-action').outerWidth(), $('.fab-action').css('width', startWidth + 'px') }, 100) // we need the font to load before we grab the button's width
-$(document).on('click', '#activate-settings', (e) => {
-  console.log(startWidth)
-  //animate
-  $('.fab-action').toggleClass('shown')
-
-  $({ deg: 0 }).animate({ deg: 180 }, {
-    duration: 250,
-    step(deg) {
-      $('#activate-settings i').css('transform', `rotate(${deg}deg)`)
-    }
-  })
-  setTimeout(() => { $('#activate-settings i').toggleClass('fa-times') }, 125)
-
-  if(!opened) {
-    $('.fab-action').animate({
-      width: 200
-    }, 250)
-
-    $('#activate-settings span').text('Close')
-
-    opened = true
-  } else {
-    $('.fab-action').animate({
-      width: startWidth
-    }, 250)
-
-    $('#activate-settings span').text('Settings')
-
-    opened = false
-  }
-})
