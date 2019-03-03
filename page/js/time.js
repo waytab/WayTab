8c041b3c4e11c424e32f1ec4240dc0ca99c7a243
Andy Wang <andy_wang@student.wayland.k12.ma.us>
2019-03-03 13:54:52 -0500

    fixed weekend behavior
    block doesnt highlight on weekends, re-added the Monday will be... day cuz IT'S FIXED FINALLY

--- page/js/time.js
+++ page/js/time.js
@@ -48,8 +48,9 @@ setInterval( () => {
 }, 1000)
 
 function updateBlock() {
+  let dayNum = parseInt(moment().format('e'))
   block = getCurrentBlock()
-  highlightBlock()
+  if(dayNum !== 0 && dayNum !== 6) highlightBlock()
   removeLetterHint()
 }
 
@@ -59,7 +60,7 @@ function displayTime() {
     if((dayNum !== 0 && dayNum !== 6) && letter !== undefined) {
       $('#time-container').html(`<span id="time-display">${moment().format('h:mm:ss')}</span>${moment().format('a')} | ${letter} Day | ${display}`)
     }else {
-      $('#time-container').html(`<span id="time-display">${moment().format('h:mm:ss')}</span>${moment().format('a')} | ${display}`)
+      $('#time-container').html(`<span id="time-display">${moment().format('h:mm:ss')}</span>${moment().format('a')} | Monday: ${letter} Day`)
     }
     
   } catch(e) {
