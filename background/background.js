c0f5ca3e16718d51da36686e9e1b607e90ebb08e
Andrew Ward <tmiandreww@gmail.com>
2019-03-06 08:50:02 -0500

    Update background.js

--- background/background.js
+++ background/background.js
@@ -5,6 +5,6 @@ chrome.runtime.onInstalled.addListener(function(deets) {
   if(deets.reason == 'install') {
     chrome.storage.sync.set({title: 'WayTab', links: tabs.tabs, background: './img/school.jpg', autoHide: false, font: 'default', enableWspn: true, bell2: false, todoDate: 'Tomorrow', startChangeLog: true, elapseForm: 'Time', dark: false, neue_schedule: true}, function() {});
   } else if (deets.reason == 'update') {
-    chrome.storage.sync.set({startChangeLog: true, neue_schedule: true}, function() {})
+    chrome.storage.sync.set({startChangeLog: true, neue_schedule: true, setup: true}, function() {})
   }
 })
