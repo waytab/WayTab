30715fe047709b0e8b226f7bb4feab3a163dcc4f
Andrew Ward <tmiandreww@gmail.com>
2021-08-17 00:33:06 -0400

    update settings.js

--- page/js/settings.js
+++ page/js/settings.js
@@ -1,35 +1,32 @@
 let opened = false
 let startWidth
-setTimeout(() => { startWidth = $('.fab-action').outerWidth(), $('.fab-action').css('width', startWidth + 'px') }, 100) // we need the font to load before we grab the button's width
-$(document).on('click', '#activate-settings, #settings-clickaway', (e) => {
-  //animate
-  $('.fab-action').toggleClass('shown')
 
-  $({ deg: 0 }).animate({ deg: 180 }, {
-    duration: 250,
-    step(deg) {
-      $('#activate-settings i').css('transform', `rotate(${deg}deg)`)
+// setTimeout(() => {
+//   startWidth = document.querySelector('.fab-action').offsetWidth
+//   //document.querySelector('.fab-action').css('width', startWidth + 'px')
+//   document.querySelector('.fab-action').style.width = (startWidth + 1 + 'px')
+// }, 100) // we need the font to load before we grab the button's width
+
+document.addEventListener('click', e => {
+  e.path.forEach(element => {
+    if(element.id == 'activate-settings' || element.id == 'settings-clickaway'){
+      document.querySelector('.fab-action').classList.toggle('shown')
+      document.querySelector('#activate-settings i').classList.toggle('fa-times')
+
+      if(!opened) {
+        document.querySelector('#activate-settings span').textContent = 'Close'
+
+        let clickawayElement = document.createElement('div')
+        clickawayElement.id = 'settings-clickaway'
+        document.body.prepend(clickawayElement)
+
+        opened = true
+      } else {
+        document.querySelector('#activate-settings span').textContent = 'Settings'
+        document.querySelector('#settings-clickaway').remove()
+
+        opened = false
+      }
     }
-  })
-  setTimeout(() => { $('#activate-settings i').toggleClass('fa-times') }, 125)
-
-  if (!opened) {
-    $('.fab-action').animate({
-      width: 200
-    }, 250)
-
-    $('#activate-settings span').text('Close')
-    $('body').prepend($('<div></div>').attr({ id: 'settings-clickaway' }))
-
-    opened = true
-  } else {
-    $('.fab-action').animate({
-      width: startWidth
-    }, 250)
-
-    $('#activate-settings span').text('Settings')
-    $('#settings-clickaway').remove()
-
-    opened = false
-  }
+  });
 })
