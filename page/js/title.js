4226b49aca1fff32f9672b4af3ac2420fefe03f2
Andrew Ward <tmiandreww@gmail.com>
2021-08-17 00:45:21 -0400

    update title.js

--- page/js/title.js
+++ page/js/title.js
@@ -1,16 +1,31 @@
+// @TO-DO
+// untested
+// should probably test before prod
+// thx
+// leaving old jquery commented bc untesting and nice to have reference to old working code
+
 export default class Title {
   constructor() {
     chrome.storage.sync.get("title", function({title}) {
-      $('#title-container').text(title)
-      $('#title-input').val(title)
+      document.querySelector('#title-container').textContent = title
+      document.querySelector('#title-input').value = title
     })
 
-    $(document).on('click', '#settings-close', function() {
-      let newtitle = $('#title-input').val()
-      chrome.storage.sync.set({title: newtitle}, function() {
-        $('#title-container').text(newtitle)
-        console.log("New Title Set")
-      })
+    document.addEventListener('click', e => {
+      if(e.target.id == '#settings-close') {
+        let newtitle = document.querySelector('#title-input').value
+        chrome.storage.sync.set({title: newtitle}, function() {
+          document.querySelector('#title-container').textContent = newtitle
+          console.log('%ctitle.js line:13 New Title Set', 'color: #007acc;', newtitle);
+        })
+      }
     })
+    // $(document).on('click', '#settings-close', function() {
+    //   let newtitle = $('#title-input').val()
+    //   chrome.storage.sync.set({title: newtitle}, function() {
+    //     $('#title-container').text(newtitle)
+    //     console.log("New Title Set")
+    //   })
+    // })
   }
 }
